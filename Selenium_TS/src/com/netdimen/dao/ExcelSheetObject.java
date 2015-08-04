9fb772a533d0c9ec9a03f1adba0db93bb7d3fb9a
martinwanghuai <dragonwanghuai@gmail.com>
2015-08-04 17:05:08 +0800

    Add object to wrap FuncName, RowNum, SheetName, Label, and Author

---
+++ Selenium_TS/src/com/netdimen/dao/ExcelSheetObject.java
@@ -0,0 +1,61 @@
+/**
+ * @author martin.wang
+ *
+ */
+
+
+package com.netdimen.dao;
+
+public class ExcelSheetObject {
+	
+	private final String sheetName;
+	
+	private final String rowNum;
+	
+	private final String funcName;
+	
+	private final String label;
+	
+	private final String author;
+	
+	public ExcelSheetObject(final String funcName, final String rowNum, final String sheetName, final String label, final String author){
+		
+		this.sheetName = sheetName;
+		this.rowNum = rowNum;
+		this.funcName = funcName;
+		this.label = label;
+		this.author = author;
+	}
+
+	
+    public String getSheetName() {
+    
+    	return sheetName;
+    }
+
+	
+    public String getRowNum() {
+    
+    	return rowNum;
+    }
+
+	
+    public String getFuncName() {
+    
+    	return funcName;
+    }
+
+	
+    public String getLabel() {
+    
+    	return label;
+    }
+
+	
+    public String getAuthor() {
+    
+    	return author;
+    }
+	
+	
+}
