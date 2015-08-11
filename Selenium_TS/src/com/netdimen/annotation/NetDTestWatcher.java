14e16e5b299534cce5f4d18c1070b560d750c5ac
martinwanghuai <dragonwanghuai@gmail.com>
2015-08-11 08:34:30 +0800

    Code refactor

--- Selenium_TS/src/com/netdimen/annotation/NetDTestWatcher.java
+++ Selenium_TS/src/com/netdimen/annotation/NetDTestWatcher.java
@@ -3,38 +3,35 @@ package com.netdimen.annotation;
 import java.lang.reflect.Method;
 
 import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 
 import com.netdimen.abstractclasses.TestObject;
 
 public abstract class NetDTestWatcher extends TestWatcher implements
 		INetDTestWatcher {
 
+	@Override
 	public abstract boolean isSkipClass(TestObject obj);
 
+	@Override
 	public abstract boolean isSkipMethod(Method method);
 
 	@Override
-	public void start(TestObject obj) {
-		// TODO Auto-generated method stub
+	public void start(final TestObject obj) {
 
 	}
 
 	@Override
-	public void failed(Throwable e, TestObject obj) {
-		// TODO Auto-generated method stub
+	public void failed(final Throwable e, final TestObject obj) {
 
 	}
 
 	@Override
-	public void succeeded(TestObject obj) {
-		// TODO Auto-generated method stub
+	public void succeeded(final TestObject obj) {
 
 	}
 
 	@Override
-	public void finished(TestObject obj) {
-		// TODO Auto-generated method stub
+	public void finished(final TestObject obj) {
 
 	}
 
